3d06dd17d2f593d5a4ae4d9106e93eb0136709bf
ryeowon hong <ryeowon13@gmail.com>
2024-10-13 00:10:30 +0900

    Add camera permission checking

--- android-app/app/src/main/java/com/example/allio/MainActivity.kt
+++ android-app/app/src/main/java/com/example/allio/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.allio
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,21 +13,31 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.allio.ui.theme.AllioTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AllioTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        CheckPermission()
+    }
+    private fun CheckPermission() {
+        val permissionCheck = ContextCompat.checkSelfPermission(
+            this, Manifest.permission.CAMERA
+        )
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf<String>(Manifest.permission.CAMERA),
+                1000
+            )
+            Toast.makeText(
+                this@MainActivity, "카메라 권한이 필요합니다.",
+                Toast.LENGTH_SHORT
+            ).show()
+            return
         }
     }
 }
