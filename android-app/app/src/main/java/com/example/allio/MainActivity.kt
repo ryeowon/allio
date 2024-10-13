1ec2fd18eed03b4ba8f5555e00e4f25c81d67382
ryeowon hong <ryeowon13@gmail.com>
2024-10-13 16:34:31 +0900

    Add capture button

--- android-app/app/src/main/java/com/example/allio/MainActivity.kt
+++ android-app/app/src/main/java/com/example/allio/MainActivity.kt
@@ -3,19 +3,12 @@ package com.example.allio
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.camera.core.Preview
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.example.allio.ui.theme.AllioTheme
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
@@ -31,6 +24,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         previewView = findViewById(R.id.previewView)
+        var captureButton = findViewById<Button>(R.id.captureButton)
 
         checkPermission()
 
