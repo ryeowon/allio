6fd60fa685125592bddfb6720cad2e495ec1027e
ryeowon hong <ryeowon13@gmail.com>
2024-10-20 22:05:29 +0900

    Set response timeout

--- android-app/app/src/main/java/com/example/allio/RetrofitSingleton.kt
+++ android-app/app/src/main/java/com/example/allio/RetrofitSingleton.kt
@@ -1,13 +1,22 @@
 import com.example.allio.MainActivity
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitSingleton {
-    private const val BASE_URL = "http://192.168.219.108:5001/" // Flask
+    private const val BASE_URL = "http://192.168.219.108:5001" // Flask
+
+    private val client: OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .readTimeout(60, TimeUnit.SECONDS)
+            .build()
+    }
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
